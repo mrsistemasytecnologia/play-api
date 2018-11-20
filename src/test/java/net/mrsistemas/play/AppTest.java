33db5c795c798f35af4fa85be8bf49d6dd89ff87
maxfredis.tovar <maxther911@gmail.com>
2018-11-20 15:49:34 -0500

    Adicion de proyectos faltantes

---
+++ src/test/java/net/mrsistemas/play/AppTest.java
@@ -0,0 +1,21 @@
+package net.mrsistemas.play;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+@SuppressWarnings("unused")
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    //@Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
